160466dc40169b22906b2aae9713f693ba4a25d8
Sy Traore <132951255+sytraore@users.noreply.github.com>
2024-08-04 22:12:45 -0400

    bug fixed

--- pantry/app/page.js
+++ pantry/app/page.js
@@ -1,12 +1,25 @@
 'use client'
 
-import Image from "next/image";
+
 import { useState, useEffect } from "react";
 import { firestore } from "@/firebase";
 import { Box, Button, Modal, Stack, TextField, Typography } from "@mui/material";
 import { collection, deleteDoc, doc, getDoc, getDocs, query, setDoc } from "firebase/firestore";
 
-
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'white',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 3,
+}
 
 
 export default function Home() {
@@ -20,10 +33,7 @@ export default function Home() {
     const docs = await getDocs(snapshot)
     const pantryList = []
     docs.forEach((doc) => {
-      pantryList.push({
-        name: doc.id,
-        ...doc.data(),
-      })
+      pantryList.push({name: doc.id, ...doc.data()})
 
     })
 
@@ -31,8 +41,8 @@ export default function Home() {
     //console.log(pantryList)
   }
 
-  const removeItem = async (name) => {
-    const docRef = doc(collection(firestore, 'pantry'), name)
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, 'pantry'), item)
     const docSnapshot = await getDoc(docRef)
     if (docSnapshot.exists()){
       const {quantity} = docSnapshot.data()
@@ -47,8 +57,8 @@ export default function Home() {
     await updatePantry()
   }
 
-  const addItem = async (name) => {
-    const docRef = doc(collection(firestore, 'pantry'), name)
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, 'pantry'), item)
     const docSnapshot = await getDoc(docRef)
     if (docSnapshot.exists()){
       const {quantity} = docSnapshot.data()
@@ -71,103 +81,88 @@ export default function Home() {
 
 
   return (
-    <Box 
-      width="100vw" 
-      height="100vh" 
+    <Box
+      width="100vw"
+      height="100vh"
       display={'flex'}
-      flexDirection={'column'}
       justifyContent={'center'}
+      flexDirection={'column'}
       alignItems={'center'}
       gap={2}
     >
-
       <Modal
         open={open}
         onClose={handleClose}
-        
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
       >
-
-        <Box
-          position={'absolute'}
-          top="50%"
-          left="50%"
-          
-          width={400}
-          bgcolor="white"
-          border={'2px solid #000'}
-          boxShadow={24}
-          p={4}
-          display={'flex'}
-          flexDirection={'column'}
-          gap={3}
-          sx={{
-            transform:"translate(-50%, -50%)"
-          }}
-        
-        >
-
-          <Typography variant="h6">Add Item</Typography>
-
-          <Stack width={300} direction={"row"} spacing={2}>
-            <TextField 
-              variant="outlined" 
-              fullWidth 
-              value={itemName}
-              onChange={(e)=>{
-                setItemName(e.target.value)
-              }}
-            
-            ></TextField>
-
-            <Button 
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add Item
+          </Typography>
+          <Stack width="100%" direction={'row'} spacing={2}>
+            <TextField
+              id="outlined-basic"
+              label="Item"
               variant="outlined"
-              onClick={()=>{
+              fullWidth
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}
+            />
+            <Button
+              variant="outlined"
+              onClick={() => {
                 addItem(itemName)
                 setItemName('')
                 handleClose()
               }}
             >
               Add
-
             </Button>
           </Stack>
-
         </Box>
-      
       </Modal>
-      
-      
-      <Button
-        variant="contained"
-        onClick={()=>{
-          handleOpen()
-        }}
-      
-      > Start</Button>
-
-      <Box border="1px solid #333">
-        <Box width="800px" height="100px" bgcolor="#ADD8E6">
-          <Typography 
-            variant="h2" 
-            color="#333" 
-            display={'flex'}
-            alignItems={'center'} 
-            justifyContent={'center'}
-            >
-            Pantry Items
+      <Button variant="contained" onClick={handleOpen}>
+        Start
+      </Button>
+      <Box border={'1px solid #333'}>
+        <Box
+          width="800px"
+          height="100px"
+          bgcolor={'#ADD8E6'}
+          display={'flex'}
+          justifyContent={'center'}
+          alignItems={'center'}
+        >
+          <Typography variant={'h2'} color={'#333'} textAlign={'center'}>
+            Items
           </Typography>
-
         </Box>
-
+        <Stack width="800px" height="500px" spacing={2} overflow={'auto'}>
+          {pantry.map(({name, quantity}) => (
+            <Box
+              key={name}
+              width="100%"
+              minHeight="150px"
+              display={'flex'}
+              justifyContent={'space-between'}
+              alignItems={'center'}
+              bgcolor={'#f0f0f0'}
+              paddingX={5}
+            >
+              <Typography variant={'h3'} color={'#333'} textAlign={'center'}>
+                {name.charAt(0).toUpperCase() + name.slice(1)}
+              </Typography>
+              <Typography variant={'h3'} color={'#333'} textAlign={'center'}>
+                Quantity: {quantity}
+              </Typography>
+              <Button variant="contained" onClick={() => removeItem(name)}>
+                Remove
+              </Button>
+            </Box>
+          ))}
+        </Stack>
       </Box>
-
-      <Stack width="800px" height="300px" spacing={2}overflow={'auto'}>
-        
-        <Typography>Hello</Typography>
-
-      </Stack>
-      
-
     </Box>
-  );
+  )
 }
